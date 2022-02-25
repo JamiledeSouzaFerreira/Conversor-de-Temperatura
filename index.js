45332876eed9ad41a67a5028456eb278cb1488a6
Jamile de Souza Ferreira <jj2003.011@gmail.com>
2022-02-25 11:33:43 -0300

    celsius pra kelvin ou farenheit

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-//var celsius = Number.parseFloat(window.prompt('Escreva a temperatura em Celsius'))
-//var kelvin = celsius + 273
-//window.alert('A temperatura corresponde a ' + kelvin + ' graus Kelvin')
 
-var celsius = document.querySelector('input.text')
 var btn = document.querySelector('input#btn')
+btn.addEventListener("click", converter)
 
+function converter (){
+    var txt = document.querySelector('input.txt')
+    var rad = document.getElementsByName('radtemp')
+    var temp = ''
+    var grau = ''
+    var res = document.querySelector('h2')
+    if (rad[0].checked){
+        var temp = txt + 273
+        var grau  = 'Kelvin'
 
-btn.addEventListener("click", ckelvin)
-btn.addEventListener("click", cfarenheit)
+    } else if (rad[1].checked){
+        var temp = (9*txt + 5*32) /5 
+        var grau = 'Farenheit'
+    }
 
-function ckelvin (){
-    let c = Number.parseFloat(celsius.value)
-    let kelvin = c+273
-    let resposta = document.querySelector('h2.k-resposta')
-    resposta.innerHTML = ('A temperatura corresponde a '+ kelvin + ' graus Kelvin')
-}
-
-function cfarenheit() {
-    let c = Number.parseFloat(celsius.value)
-    let f = [9*c + (5*32)]/5
-    let resposta = document.querySelector('h2.f-resposta')
-    resposta.innerHTML = ('A temperatura corresponde a '+ f + ' graus Farenheit')
+    res.innerHTML = (`A temperatura corresponde a ${temp} graus ${grau}`)
 }
